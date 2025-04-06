a9127845198d72389c86537e5a603c89e92d2de2
Rujroot <rujroot@gmail.com>
2025-04-06 16:31:32 +0700

    update app.js and app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,10 @@
 const { dayOfTheWeek } = require('./app');
+const { monthOfTheYear } = require('./app');
 
+test('getDay returns the month of the year', () => {
+    const month = monthOfTheYear( new Date('3/11/2020') );
+    expect( month ).toBe('November');
+});
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
